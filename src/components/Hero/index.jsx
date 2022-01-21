5b4e2948314856c5c946f3dac9f5186c7317dd2c
Sera Zenginler <serazenginler@outlook.com>
2022-01-21 22:16:40 +0300

    See Product Btn is added.

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,11 +1,12 @@
 import styles from "./style.module.scss"
+import SeeProductBtn from "../SeeProductBtn"
 
 const Hero = () => {
   return <section className={styles.container}>
     <p className="overline">New Product</p>
     <h1>XX99 Mark II Headphones</h1>
     <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-    <a href="">See Product</a>
+    <SeeProductBtn link="#"/>
   </section>;
 };
 
---
+++ src/components/SeeProductBtn/index.jsx
@@ -0,0 +1,7 @@
+const SeeProductBtn = ({link}) => {
+  return (
+    <a href={link} className="product-btn">See Product</a>
+  )
+}
+
+export default SeeProductBtn;
