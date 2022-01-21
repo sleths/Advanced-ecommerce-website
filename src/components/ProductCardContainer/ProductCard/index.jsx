f0b6d8a2abc51c540382e583756c9178a1d7a469
Sera Zenginler <serazenginler@outlook.com>
2022-01-22 01:48:49 +0300

    Product components for mobile.

--- src/components/ProductCardContainer/ProductCard/index.jsx
+++ src/components/ProductCardContainer/ProductCard/index.jsx
@@ -1,4 +1,4 @@
-import right from "../../../assets/icons/icon-arrow-right.svg";
+import arrowRight from "../../../assets/icons/icon-arrow-right.svg";
 import styles from "./style.module.scss";
 
 const ProductCard = ({src, alt, title, link}) => {
@@ -6,8 +6,9 @@ const ProductCard = ({src, alt, title, link}) => {
     <section className={`${styles.container} centered`}>
       <img src={src} alt={alt} />
       <h5>{title}</h5>
-      <div>
+      <div className={styles.btnContainer}>
       <a href={link}>Shop</a>
+      <img src={arrowRight} alt="Click for seeing" />
       </div>
     </section>
   )
