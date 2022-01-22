ddc88ef0859350f412596e9f3351eec65a07d0ab
Sera Zenginler <serazenginler@outlook.com>
2022-01-23 01:42:06 +0300

    New card components are added.

--- src/components/HomeAdComponent/index.jsx
+++ src/components/HomeAdComponent/index.jsx
@@ -1,5 +1,6 @@
 import styles from "./style.module.scss";
 import orangeSpeakerImg from "../../assets/home/mobile/image-speaker-zx9.png";
+import earPhoneImg from "../../assets/home/mobile/image-earphones-yx1.jpg";
 import SeeProductBtn from "../SeeProductBtn";
 
 const HomeAdComponent = () => {
@@ -10,6 +11,19 @@ const HomeAdComponent = () => {
         <h1>ZX9 speaker</h1>
         <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
         <SeeProductBtn link="" type="black" />
+      </div>
+
+      <div className={`${styles.graySpeaker} centered`}>
+        <h1>ZX7 speaker</h1>
+        <SeeProductBtn link="" type="bordered" />
+      </div>
+
+      <div className={`${styles.earPhones} centered`}>
+        <img src={earPhoneImg} alt="Earphones" />
+      <div className={`${styles.grayEarphones}`}>
+      <h1>YX1 earphones</h1>
+      <SeeProductBtn link="" type="bordered" />
+      </div>
       </div>
     </section>
   );
