73a0bcf414a758b1e5d9f4789f39c8befd58fd99
Sera Zenginler <serazenginler@outlook.com>
2022-01-28 12:51:09 +0300

    Hero is made responsive for Desktop.

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,10 +3,12 @@ import SeeProductBtn from "../SeeProductBtn"
 
 const Hero = () => {
   return <section className={`${styles.container} centered`}>
+    <div className={styles.textContainer}>
     <p className="overline">New Product</p>
     <h1 className={styles.heading}>XX99 Mark II Headphones</h1>
     <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
     <SeeProductBtn link="#" type="orange"/>
+    </div>
   </section>;
 };
 
