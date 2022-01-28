5c8f3386d4c507441277253661f331c9516032cf
Sera Zenginler <serazenginler@outlook.com>
2022-01-28 19:38:29 +0300

    React router and links are added.

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,27 +3,52 @@ import logo from "../../assets/icons/logo.svg";
 import hmbrgIcon from "../../assets/icons/icon-hamburger.svg";
 import cartIcon from "../../assets/icons/icon-cart.svg";
 import ProductCardContainer from "../ProductCardContainer";
+import { Link } from "react-router-dom";
 
 import styles from "./style.module.scss";
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const handleClick = () => setIsNavOpen((prev) => !prev);
+  const navLinks = [
+    {
+      name: "home",
+      to: "/",
+    },
+    {
+      name: "headphones",
+      to: "/headphones",
+    },
+    {
+      name: "speakers",
+      to: "/speakers",
+    },
+    {
+      name: "earphones",
+      to: "/earphones",
+    },
+  ];
   return (
     <>
-    <header className={styles.header}>
-      <nav className={styles.container}>
-        <img
-          src={hmbrgIcon}
-          alt="Open the mobile menu."
-          onClick={handleClick}
-        />
-        <img src={logo} alt="Company logo" />
-        <img src={cartIcon} alt="Go to the card." />
-      </nav>
-      {isNavOpen && <ProductCardContainer isOnNav />}
-    </header>
-    {isNavOpen &&  <div className={styles.overlay}></div>}
+      <header className={styles.header}>
+        <nav className={styles.container}>
+          <img
+            src={hmbrgIcon}
+            alt="Open the mobile menu."
+            onClick={handleClick}
+          />
+          <img src={logo} alt="Company logo" />
+          <ul className={styles.list}>
+            {navLinks.map((link) => (
+              <li key={link.name}><Link to={link.to}>{link.name}</Link></li>
+            )
+            )}
+          </ul>
+          <img src={cartIcon} alt="Go to the card." />
+        </nav>
+        {isNavOpen && <ProductCardContainer isOnNav />}
+      </header>
+      {isNavOpen && <div className={styles.overlay}></div>}
     </>
   );
 };
