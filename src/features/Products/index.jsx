0f73755efba5baac0036b5e42f9d096bea0a2868
Sera Zenginler <serazenginler@outlook.com>
2022-01-31 22:12:25 +0300

    Redux

---
+++ src/features/Products/index.jsx
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const productSlice = createSlice({
+  name: "products",
+  initalState: {value: []},
+  reducers: {
+    addToCart: (state, action)  => {
+      
+    }
+  }
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.scss";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+const store = configureStore( {
+  reducer: {}
+} )
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>   
-    <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
