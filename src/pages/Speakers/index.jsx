d8e7bad3646dc9157427757a58b2ea88876b6d5c
Sera Zenginler <serazenginler@outlook.com>
2022-02-04 22:24:26 +0300

    Speakers page

--- src/pages/Speakers/index.jsx
+++ src/pages/Speakers/index.jsx
@@ -1,10 +1,15 @@
 import ProductHero from "../../components/ProductHero";
+import SingleProductCard from "../../components/SingleProductCard";
+import { speakers } from "../../utils/constants";
 
 const Speakers = () => {
-  return (
-    <>
-    <ProductHero title="Speakers"/>
-    </>
+return (
+<>
+<ProductHero title="Speakers"/>
+<main>
+{speakers.map((product) => ( <SingleProductCard title={product.name} src={product.image.desktop} description={product.description} key={product.id}/>))}
+</main>
+</>
 );
 };
 
