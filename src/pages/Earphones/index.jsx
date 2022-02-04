74d1c5d5c99b00daa6371c16319c67bff8567286
Sera Zenginler <serazenginler@outlook.com>
2022-02-04 22:32:05 +0300

    Earphones page is added.

--- src/pages/Earphones/index.jsx
+++ src/pages/Earphones/index.jsx
@@ -1,9 +1,22 @@
 import ProductHero from "../../components/ProductHero";
+import SingleProductCard from "../../components/SingleProductCard";
+import { earphones } from "../../utils/constants";
 
 const Earphones = () => {
   return (
     <>
     <ProductHero title="Earphones"/>
+    <main className="product-page-container">
+      {earphones.map((product) => (
+        <SingleProductCard 
+        title={product.name}
+        description={product.description}
+        src={product.image.desktop}
+        key={product.id}
+        />
+      ))}
+
+    </main>
     </>
 );
 };
--- src/pages/Speakers/index.jsx
+++ src/pages/Speakers/index.jsx
@@ -3,14 +3,21 @@ import SingleProductCard from "../../components/SingleProductCard";
 import { speakers } from "../../utils/constants";
 
 const Speakers = () => {
-return (
-<>
-<ProductHero title="Speakers"/>
-<main>
-{speakers.map((product) => ( <SingleProductCard title={product.name} src={product.image.desktop} description={product.description} key={product.id}/>))}
-</main>
-</>
-);
+  return (
+    <>
+      <ProductHero title="Speakers" />
+      <main className="product-page-container">
+        {speakers.map((product) => (
+          <SingleProductCard
+            title={product.name}
+            src={product.image.desktop}
+            description={product.description}
+            key={product.id}
+          />
+        ))}
+      </main>
+    </>
+  );
 };
 
 export default Speakers;
