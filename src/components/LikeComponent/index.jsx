a6fbc53744e70907c8aaae07d5c0ef75dbebacde
Sera Zenginler <serazenginler@outlook.com>
2022-02-08 23:35:37 +0300

    Likes Component is added.

---
+++ src/components/LikeComponent/index.jsx
@@ -0,0 +1,10 @@
+import xx99Image from "../../assets/product-xx99-mark-one-headphones/desktop/image-product.jpg";
+import xx59Image from "../../assets/product-xx59-headphones/desktop/image-product.jpg";
+
+const LikeComponent = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default LikeComponent;
